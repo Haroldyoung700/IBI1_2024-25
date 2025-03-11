690607228b3a07f78c8fb731e377715596bc062a
Haroldyoung700 <haoran.24@intl.zju.edu.cn>
2025-03-11 11:48:47 +0800

    commit for Practical 4

--- Practical4/bmi_calculator.py
+++ Practical4/bmi_calculator.py
@@ -1,10 +1,10 @@
-weight = float (input ("Enter your weight in kg: "))
-height = float (input ("Enter your height in meters: "))
-bmi = weight / (height ** 2)
-print ("Your BMI is: ", bmi)
+weight = float (input ("Enter your weight in kg: "))      #Get weight from user
+height = float (input ("Enter your height in meters: "))  #Get height from user
+bmi = weight / (height ** 2)                              #Calculate BMI
+print ("Your BMI is: ", bmi)                              #Print BMI
 if bmi < 18.5:
     print ("You are underweight")
 elif bmi >= 18.5 and bmi <= 30:
     print ("You are normal weight")
 else:
-    print ("You are obese")
+    print ("You are obese")                               #Print the BMi condition
