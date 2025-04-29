f21be7c7d3aadf43293ab3acda3573c621b28da1
Haroldyoung700 <haoran.24@intl.zju.edu.cn>
2025-04-29 16:21:11 +0800

    commit

--- Practical10/File dalys.py
+++ Practical10/File dalys.py
@@ -5,21 +5,24 @@ import numpy as np
 
 os.chdir("/Users/harold_young/Documents/GitHub/IBI1_2024-25/Practical10")
 dalys_data = pd.read_csv("dalys-rate-from-all-causes.csv")
+
+
 # Task1: show the year for the first 10 rows
 years_first_10 = dalys_data.iloc[0:10, 2]  # column loc matching the year
 print("\n the year for the first 10 rows：\n", years_first_10)
-    # gather the 10th year with DALYs data recorded in Afghanistan
+# gather the 10th year with DALYs data recorded in Afghanistan
 afghan_data = dalys_data[dalys_data['Entity'] == 'Afghanistan'].reset_index(drop=True)
 tenth_year_afghan = afghan_data.loc[9, 'Year'] 
 print(f"\n the 10th year with DALYs data recorded in Afghanistan：{tenth_year_afghan}")
 print("Data column name：", dalys_data.columns.tolist())  
-    # Filter the data for 1990
+# Filter the data for 1990
 mask_1990 = dalys_data['Year'] == 1990
 dalys_1990 = dalys_data.loc[mask_1990, ['Entity', 'DALYs']]  
 print("\n1990 data：\n", dalys_1990.head())
-#=======================================
+
+
 # Task2: Calculate the mean of uk and france and conduct the comparison 
-    # Calculate the mean for uk and france
+# Calculate the mean for uk and france
 uk_data = dalys_data[dalys_data['Entity'] == 'United Kingdom']
 france_data = dalys_data[dalys_data['Entity'] == 'France']
 mean_uk = uk_data['DALYs'].mean()          
@@ -27,9 +30,10 @@ mean_france = france_data['DALYs'].mean()
 print(f"\nUK mean DALYs：{mean_uk:.2f}")
 print(f"France mean DALYs：{mean_france:.2f}")
 print("UK mean" + (" higher than " if mean_uk > mean_france else " lower than ") + "France")
-#=======================================
+
+
 # Task3: print a plot describing the trend of uk data
-    # Plot the figure
+# Plot the figure
 plt.figure(figsize=(10,6))
 plt.plot(uk_data['Year'], uk_data['DALYs'], label='United Kingdom')  
 plt.title('DALYs Over Time in the UK')
@@ -39,9 +43,10 @@ plt.legend()
 plt.tight_layout()   
 plt.show()           
 plt.savefig('uk_dalys_trend.png', dpi=300)
-#=======================================
-# Task4: Self-designed question: Compare the mean DALY data of heterogeneous regions in terms of economic development
-    # define the region by representative country
+
+
+# Task4: Self-designed question: Compare the mean DALY data of different regions in terms of economic development
+# define the region by representative country
 def assign_region(entity):
     if entity in ['Afghanistan', 'India', 'Pakistan']:
         return 'South Asia'
